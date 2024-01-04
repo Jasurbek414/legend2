8978d350a15a07b39ddcdbab3ec3937fcab509b4
Jasurbek414 <muminovjasurbek12@gmail.com>
2024-01-04 14:49:38 +0500

    home

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -3,29 +3,53 @@ import { Card, Col, Row } from "antd";
 import axios from "axios";
 
 function Home() {
+  const [itemCategories, setItemCategories] = useState("");
+  const [materialCategories, setMaterialCategories] = useState("");
+  const [items, setItems] = useState("");
+  const [materials, setMaterials] = useState("");
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    // Fetch data from the API endpoint
+    axios.get("api/home")
+      .then((response) => {
+        // Assuming the response data has properties like productCategory, materialCategory, productCount, and materialCount
+        // setData(response.data)
+        // console.log(data);
+        setItemCategories(response.data.itemCategories);
+        setMaterialCategories(response.data.materialCategories);
+        setItems(response.data.items);
+        setMaterials(response.data.materials);
+        // console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []); // The empty dependency array ensures this effect runs once after the initial render
+
   return (
     <div className="mt-5 grid sm:grid-cols-2 md:grid-cols-3 gap-3">
-      <Col >
+      <Col>
         <Card title="Product" bordered={false}>
-          Category soni: 
+          Category: {itemCategories}
         </Card>
       </Col>
-      <Col >
+      <Col>
         <Card title="Material" bordered={false}>
-          Category soni: 
+          Category: {materialCategories}
         </Card>
       </Col>
-      <Col >
+      <Col>
         <Card title="Product" bordered={false}>
-          Mahsulotlar soni: 
+          Count: {items}
         </Card>
       </Col>
-      <Col >
+      <Col>
         <Card title="Material" bordered={false}>
-          Mahsulotlar soni: 
+          Count: {materials}
         </Card>
       </Col>
-     
     </div>
   );
 }
